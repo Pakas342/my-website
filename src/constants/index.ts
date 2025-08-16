29d243412398ee8fda3d1a2bfdc8d8e55caca801
Juan Barrera <jcbp1999@gmail.com>
2025-08-17 00:37:25 +1000

    initial layout

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+import type { GeneralInfo, SocialLink } from "../types";
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  { name: "Linkedin", href: "https://linkedin.com/in/your-profile" },
+  { name: "Github", href: "https://github.com/your-username" },
+  { name: "Strava", href: "https://strava.com/athletes/your-id" },
+];
+
+export const GENERAL_INFO: GeneralInfo = {
+  name: "Juan Barrera",
+  title: "Poetical justice to my soul",
+  description:
+    "This is a digital space I created with the intention of sharing the things that bring me joy, and the projects I destine my time to. The more you know, the more you love.",
+  location: "Currently at Sydney, Australia",
+};
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface SocialLink {
+  name: string;
+  href: string;
+  icon?: string;
+}
+
+export interface GeneralInfo {
+  name: string;
+  title: string;
+  description: string;
+  location: string;
+}
+
